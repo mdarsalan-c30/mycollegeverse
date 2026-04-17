0b7db3a50b67cdc6878fd4e2e95c7fdae62e8543
MD ARSALAN <mdarsalan@vibein.com>
2026-04-17 18:16:59 +0530

    fix: resolve 500 server error in Admin NoteController by adding missing imports

--- app/Http/Controllers/Admin/NoteController.php
+++ app/Http/Controllers/Admin/NoteController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Subject;
 use App\Models\Course;
+use App\Models\Note;
 use App\Traits\GeneratesAiContent;
+use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
