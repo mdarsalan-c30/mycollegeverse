5caaf4efa90454c905b1489ce2afec190f254cdf
MD ARSALAN <mdarsalan@vibein.com>
2026-04-26 01:39:52 +0530

    Fix: Remove duplicate jobPostings() method from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,10 +55,6 @@ class User extends Authenticatable
         return $this->belongsTo(\App\Models\Course::class);
     }
 
-    public function jobPostings()
-    {
-        return $this->hasMany(\App\Models\JobPosting::class, 'recruiter_id');
-    }
 
     public function academicEvents()
     {
