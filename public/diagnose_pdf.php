a136a320a26c7d3b0a9e8c1c9e0690285286e39e
MD ARSALAN <mdarsalan@vibein.com>
2026-05-10 21:59:29 +0530

    Fixed diagnostic script (removed shell_exec).

--- public/diagnose_pdf.php
+++ public/diagnose_pdf.php
@@ -6,7 +6,6 @@ $results = [
     'imagick' => extension_loaded('imagick'),
     'gd' => extension_loaded('gd'),
     'php_version' => PHP_VERSION,
-    'ghostscript' => shell_exec('gs --version') ? true : false,
 ];
 
 echo json_encode($results);
