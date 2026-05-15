9e98adfa9fb0bee6d3ff25835b9cc337d09ab818
MD ARSALAN <mdarsalan@vibein.com>
2026-05-16 01:06:31 +0530

    🔧 Hotfix: Added missing namespace to MockInterviewController to resolve 500 error

--- app/Http/Controllers/MockInterviewController.php
+++ app/Http/Controllers/MockInterviewController.php
@@ -1,9 +1,11 @@
 <?php
+namespace App\Http\Controllers;
 
 use App\Models\InterviewSession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
+
 
 class MockInterviewController extends Controller
 {
